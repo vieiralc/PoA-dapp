6b88e37417f33b4d559a7ca5890f211995037bbb
Lucas Vieira <--replace-all>
2019-05-29 14:34:08 -0300

    change file name

--- webapp/apis/products/products.js
+++ webapp/apis/products/products.js
@@ -7,7 +7,7 @@ const atob = require("atob");
 const product_abi = require(path.resolve("../dapp/build/contracts/MyContract.json"));
 const httpEndpoint = 'http://localhost:8540';
 
-let contractAddress = '0x5B6a6Df167cb5DA753fa0eB5BA27f7cbA34f4524';
+let contractAddress = '0xe99789A2367F08fEB5ba9553bA54C14C63Ccb583';
 
 let web3 = new Web3(httpEndpoint);
 let MyContract = new web3.eth.Contract(product_abi.abi, contractAddress);
@@ -32,7 +32,7 @@ module.exports = {
             res.render('listaProdutos.html');
         }
     },
-    listProducts: async function(req, res) {
+    getProducts: async function(req, res) {
 
         let userAddr = req.session.address;
         console.log(userAddr);
@@ -102,47 +102,5 @@ module.exports = {
                 return res.send({ 'error': true, 'msg': 'Erro ao desbloquear sua conta. Por favor, tente novamente mais tarde.'});
             }
         }
-    },
-    addProductToStage: async function(req, res) {
-        console.log("*** products -> productsApi -> addProductToStage ***");
-        console.dir(req.body);
-
-        let productsIds = [];
-        let stageDesc = req.body.StageDesc;
-        let userAddr = req.session.address;
-        let pass = req.session.password;
-
-        // converter ids dos produtos para inteiro
-        for (let i = 0; i < req.body.productsIds.length; i++) {
-            let productId = parseInt(req.body.productsIds[i], 10);
-            productsIds.push(productId);
-        }
-
-        console.log(userAddr, productsIds, stageDesc);
-
-        // unlock account
-        await web3.eth.personal.unlockAccount(userAddr, pass, null)
-            .then(async result => {
-                console.log("Conta desbloqueada: ", result);
-                // salvar etapa no contrato
-                await MyContract.methods.addToStage(productsIds, stageDesc)
-                    .send({ from: userAddr, gas: 3000000 })
-                    .then(res => {
-                        console.log("etapa registrada com sucesso");
-                        console.log(res);
-                        res.send({ error: false, msg: "Etapa registrada com sucesso"});        
-                    })
-                    .catch(err => {
-                        console.log("*** ERROR: products -> productsApi -> addProductToStage ***");
-                        console.log(err);
-                        res.send({ error: true, msg: "Erro ao registrar etapa. Por favor, tente novamente mais tarde."});
-                    })
-            })
-            .catch(err => {
-                console.log("*** ERROR: products -> productsApi -> addProductToStage ***");
-                console.log(err);
-                res.send({ error: true, msg: "Erro ao desbloquear a conta. Por favor, tente novamente mais tarde."});
-            })
-        
     }
 }
