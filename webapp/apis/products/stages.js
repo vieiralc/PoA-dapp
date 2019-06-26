f620a89ffa22e8d6d2417929f0f5d4e7cc9f00de
Lucas Vieira <--replace-all>
2019-06-26 15:16:17 -0300

    add private route

--- webapp/apis/products/stages.js
+++ webapp/apis/products/stages.js
@@ -24,45 +24,51 @@ function renderGetStages(req, res) {
 
 async function addStage(req, res) {
     
-    console.log("*** products -> productsApi -> addProductToStage ***");
-    console.dir(req.body);
+    if (!req.session.username) {
+        res.redirect('/api/auth');
+        res.end();
+    } else {
 
-    let productsIds = [];
-    let stageDesc = req.body.StageDesc;
-    let userAddr = req.session.address;
-    let pass = req.session.password;
+        console.log("*** products -> productsApi -> addProductToStage ***");
+        console.dir(req.body);
 
-    // converter ids dos produtos para inteiro
-    for (let i = 0; i < req.body.productsIds.length; i++) {
-        let productId = parseInt(req.body.productsIds[i], 10);
-        productsIds.push(productId);
+        let productsIds = [];
+        let stageDesc = req.body.StageDesc;
+        let userAddr = req.session.address;
+        let pass = req.session.password;
+
+        // converter ids dos produtos para inteiro
+        for (let i = 0; i < req.body.productsIds.length; i++) {
+            let productId = parseInt(req.body.productsIds[i], 10);
+            productsIds.push(productId);
+        }
+
+        console.log(userAddr, productsIds, stageDesc);
+        
+        // unlock account
+        await web3.eth.personal.unlockAccount(userAddr, pass, null)
+            .then(async result => {
+                console.log("Conta desbloqueada: ", result);
+                // salvar etapa no contrato
+                await MyContract.methods.addToStage(productsIds, stageDesc)
+                    .send({ from: userAddr, gas: 3000000 })
+                    .then(response => {
+                        console.log("etapa registrada com sucesso");
+                        console.log(response);
+                        res.send({ error: false, msg: "Etapa registrada com sucesso"});        
+                    })
+                    .catch(err => {
+                        console.log("*** ERROR: products -> productsApi -> addProductToStage ***");
+                        console.log(err);
+                        res.send({ error: true, msg: "Erro ao registrar etapa. Por favor, tente novamente mais tarde."});
+                    })
+            })
+            .catch(err => {
+                console.log("*** ERROR: products -> productsApi -> addProductToStage ***");
+                console.log(err);
+                res.send({ error: true, msg: "Erro ao desbloquear a conta. Por favor, tente novamente mais tarde."});
+            })
     }
-
-    console.log(userAddr, productsIds, stageDesc);
-    
-    // unlock account
-    await web3.eth.personal.unlockAccount(userAddr, pass, null)
-        .then(async result => {
-            console.log("Conta desbloqueada: ", result);
-            // salvar etapa no contrato
-            await MyContract.methods.addToStage(productsIds, stageDesc)
-                .send({ from: userAddr, gas: 3000000 })
-                .then(response => {
-                    console.log("etapa registrada com sucesso");
-                    console.log(response);
-                    res.send({ error: false, msg: "Etapa registrada com sucesso"});        
-                })
-                .catch(err => {
-                    console.log("*** ERROR: products -> productsApi -> addProductToStage ***");
-                    console.log(err);
-                    res.send({ error: true, msg: "Erro ao registrar etapa. Por favor, tente novamente mais tarde."});
-                })
-        })
-        .catch(err => {
-            console.log("*** ERROR: products -> productsApi -> addProductToStage ***");
-            console.log(err);
-            res.send({ error: true, msg: "Erro ao desbloquear a conta. Por favor, tente novamente mais tarde."});
-        })
 
 }
 
